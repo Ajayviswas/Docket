3fd2708d1eac53b5b5d09c83a3bb5ed2fe40d8fe
ajay <ajayviswas19august@gmail.com>
2023-03-18 00:23:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+mongoose.connect("mongodb://127.0.0.1:27017/ajay", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+
+
+
+
+
+
+// routes
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
+
+
+
+
+
+
+// server configurations....
+app.listen(3000, () => console.log("Server started listening on port: 3000"));
